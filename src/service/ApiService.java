5c3e5f52da84b1add0eb639efc6d0d03b9357a17
d27pomaramirez-lab <d27poma.ramirez@gmail.com>
2026-03-02 14:17:33 -0500

    servicio para procesar la API y devolver las tasas

---
+++ src/service/ApiService.java
@@ -0,0 +1,40 @@
+package service;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ApiService {
+    private static final String API_KEY = "2d25552d6b1fac7d6da6745d";
+    private static final String URL_BASE = "https://v6.exchangerate-api.com/v6/";
+
+    public double obtenerTasaDeCambio(String monedaBase, String monedaDestino) {
+        URI direccion = URI.create(URL_BASE + API_KEY + "/latest/" + monedaBase);
+
+        //Creando cliente HTTP
+        HttpClient cliente = HttpClient.newHttpClient();
+
+        //Creando la solicitud
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(direccion)
+                .build();
+
+        try {
+            //Enviando la solicitud y recibiendo respuesta
+            HttpResponse<String> response = cliente
+                    .send(request, HttpResponse.BodyHandlers.ofString());
+
+            JsonElement elemento = JsonParser.parseString(response.body());
+            JsonObject jsonObject = elemento.getAsJsonObject();
+            JsonObject tasas = jsonObject.getAsJsonObject("conversion_rates");
+            return tasas.get(monedaDestino).getAsDouble();
+
+        }catch (Exception e){
+            throw new RuntimeException("Error al conectar con la API: " + e.getMessage());
+        }
+    }
+}
