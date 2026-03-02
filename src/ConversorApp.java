b29f903f3abb4c1b4a28a4ebb7dad9a4cfd07c1e
d27pomaramirez-lab <d27poma.ramirez@gmail.com>
2026-03-02 14:19:29 -0500

    lógica de conversión de moneda y menú en consola

---
+++ src/ConversorApp.java
@@ -0,0 +1,10 @@
+
+import model.Conversor;
+
+
+public class ConversorApp {
+    public static void main(String[] args) {
+        Conversor conversor = new Conversor();
+        conversor.exibirMenu();
+    }
+}
---
+++ src/model/Conversor.java
@@ -0,0 +1,100 @@
+package model;
+
+import service.ApiService;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Conversor {
+    private ApiService ApiServicio = new ApiService();
+
+
+    private double calcularConversion(double cantidad, double tasa) {
+        return cantidad * tasa;
+    }
+
+    public void exibirMenu(){
+        Scanner lectura = new Scanner(System.in);
+        int opcion = 0;
+
+        String menu = """
+                ************************************************************
+                Bienvenido al Conversor de Monedas
+                
+                1) Dólar (USD) => Soles (PEN)
+                2) Soles (PEN) => Dólar (USD)
+                3) Dólar (USD) => Peso Argentino (ARS)
+                4) Peso Argentino (ARS) => Dólar (USD)
+                5) Dólar (USD) => Real Brasileño (BRL)
+                6) Real Brasileño (BRL) => Dólar (USD)
+                7) Dólar (USD) => Peso Colombiano (COP)
+                8) Peso Colombiano (COP) => Dólar (USD)
+                9) Salir
+                ************************************************************
+                Elija una opción válida:
+                """;
+
+        while (opcion != 9) {
+            System.out.println(menu);
+            try {
+                opcion = lectura.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Por favor, ingrese solo números enteros para el menú.");
+                lectura.nextLine();
+                continue;
+            }
+
+            if (opcion == 9) {
+                System.out.println("Finalizando programa...");
+                break;
+            }
+
+            String monedaBase = "";
+            String monedaDestino = "";
+
+            switch (opcion) {
+                case 1 -> { monedaBase = "USD"; monedaDestino = "PEN"; }
+                case 2 -> { monedaBase = "PEN"; monedaDestino = "USD"; }
+                case 3 -> { monedaBase = "USD"; monedaDestino = "ARS"; }
+                case 4 -> { monedaBase = "ARS";  monedaDestino = "USD"; }
+                case 5 -> { monedaBase = "USD"; monedaDestino = "BRL"; }
+                case 6 -> { monedaBase = "BRL"; monedaDestino = "USD"; }
+                case 7 -> { monedaBase = "USD"; monedaDestino = "COP"; }
+                case 8 -> { monedaBase = "COP"; monedaDestino = "USD"; }
+                default -> {
+                    System.out.println("Opción no válida. Intente de nuevo");
+                    continue;
+                }
+            }
+
+            double cantidad = 0;
+            boolean cantidadValida = false;
+
+            while (!cantidadValida) {
+                System.out.println("Ingrese el valor que deseas convertir:");
+                try {
+                    cantidad = lectura.nextDouble();
+                    if (cantidad <= 0) {
+                        System.out.println(" El monto a convertir debe ser mayor a cero. Intente de nuevo.\n");
+                    } else {
+                        cantidadValida = true; // Salimos del bucle solo si es válido y > 0
+                    }
+                } catch (InputMismatchException e) {
+                    System.out.println("Formato de número inválido. Use números y comas/puntos decimales según su configuración regional.");
+                    lectura.nextLine();
+                }
+            }
+
+            try{
+                double tasa = ApiServicio.obtenerTasaDeCambio(monedaBase,monedaDestino);
+                double resultado = calcularConversion(cantidad, tasa);
+
+                System.out.printf("El valor %.2f [%s] corresponde al valor final de => %.2f [%s]\n\n",
+                        cantidad, monedaBase, resultado, monedaDestino);
+
+            }catch(Exception e){
+                System.out.println("Ocurrió un error: " + e.getMessage());
+            }
+        }
+        lectura.close();
+    }
+}
